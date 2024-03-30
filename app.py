1cd01286dc90fa20c0bbababff6b6294fe1495d6
jayraj2498 <jayraj2498@gmail.com>
2024-03-30 21:41:07 +0530

    trigger the apprunner

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from src.pipeline.prediction_pipeline import CustomData , PredictPipeline
 application = Flask(__name__) 
 app=application 
 
+
 @app.route('/') 
 def home_page():
     return render_template('index.html') 
